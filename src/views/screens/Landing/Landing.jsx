79853eb1dcac8b0eb6e6cfcdeb7daaeb76f37352
Solomon <mcbryansolomon@yahoo.com>
2021-07-06 00:48:55 +0000

    Done With Landing Screen Design

--- src/views/screens/Landing/Landing.jsx
+++ src/views/screens/Landing/Landing.jsx
@@ -1,17 +1,56 @@
 import React, { useState, useContext } from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+  Image
+} from "react-native";
 import styles from "./styles";
-import { EventRegister } from "react-native-event-listeners";
 import themeContext from "../../../config/themeContext";
+import bg from "../../../../assets/images/landing.jpg";
+import logo from '../../../../assets/images/logo.png'
+import { Feather } from "@expo/vector-icons";
 
 const Landing = ({ navigation }) => {
   const theme = useContext(themeContext);
 
   return (
-    <View style={[styles.mainConatiner, { backgroundColor: theme.background }]}>
-      <Text style={[styles.text,{ color: theme.color }]}>Landing</Text>
-      <Button title="Go To Tabs" onPress={() => navigation.navigate("tabs")} />
-    </View>
+    <ImageBackground
+      source={bg}
+      style={StyleSheet.absoluteFillObject}
+      resizeMode="cover"
+    >
+      <StatusBar barStyle="light-content" />
+      <View style={styles.overlay}>
+        <View>
+          <Image source={logo} style={styles.logo}/>
+        </View>
+        <View style={styles.mainTextContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.textOne}>Find Your Next</Text>
+            <Text style={styles.textTwo}>Great Read!</Text>
+            <Text style={styles.textThree}>
+              We've got brand new books you will love
+            </Text>
+          </View>
+          <View style={styles.googleContainer}>
+            <Text style={styles.googleContainerText}>Continue With Google</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('tabs')}>
+              <View style={styles.googleButtonViewOne}>
+                <View style={styles.googleButtonViewTwo}>
+                  <View style={styles.googleButtonViewThree}>
+                    <Feather name="arrow-right" size={24} color="white" />
+                  </View>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
 
--- src/views/screens/Landing/styles.js
+++ src/views/screens/Landing/styles.js
@@ -1,9 +1,7 @@
-import { useContext } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 import { colors, dimensions } from "../../../constants";
-import themeContext from "../../../config/themeContext";
-import theme from "../../../config/theme";
 
+const win = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   mainConatiner: {
@@ -11,9 +9,83 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
- text:{
-     fontFamily: 'Bold'
- }
+  text: {
+    fontFamily: "Bold",
+  },
+  Image: {
+    width: win.width / 2,
+    height: win.width / 2,
+    resizeMode: "contain",
+    alignSelf: "center",
+    borderWidth: 1,
+    borderRadius: 20,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0,0,0,0.2)",
+  },
+  mainTextContainer: {
+    position: "absolute",
+    bottom: 0,
+    marginHorizontal: dimensions.margin.lg,
+    marginBottom: dimensions.margin.xxxl,
+  },
+  textContainer: {
+    marginBottom: dimensions.margin.xxl,
+  },
+  textOne: {
+    fontFamily: "Bold",
+    fontSize: 40,
+    color: colors.white,
+  },
+  textTwo: {
+    fontFamily: "Bold",
+    fontSize: 40,
+    color: colors.blue,
+  },
+  textThree: {
+    fontFamily: "Regular",
+    fontSize: 18,
+    color: colors.white,
+    textTransform: "capitalize",
+  },
+  googleContainer:{
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  googleContainerText:{
+    fontFamily: "Bold",
+    color: colors.white,
+    fontSize: 20
+  },
+  googleButtonViewOne:{
+    backgroundColor:"rgba(0, 172, 238, 0.4)",
+    height: dimensions.image.md,
+    width: dimensions.image.md,
+    borderRadius: dimensions.radius.lg,
+   justifyContent: 'center',
+   alignItems: 'center'
+  },
+  googleButtonViewTwo:{
+    backgroundColor:"rgba(0, 172, 238, 0.6)",
+    height: dimensions.image.md - 10,
+    width: dimensions.image.md - 10,
+    borderRadius: dimensions.radius.lg,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  googleButtonViewThree:{
+    backgroundColor:colors.blue,
+    height: dimensions.image.md - 25,
+    width: dimensions.image.md - 25,
+    borderRadius: dimensions.radius.lg,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo:{
+    height: dimensions.image.lg - 15,
+    width: dimensions.image.lg - 15
+  }
 });
 
 export default styles;
